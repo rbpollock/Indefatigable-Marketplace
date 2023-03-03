a7483d160423b16d19fee77d0944589569c66036
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2023-03-02 23:46:56 -0600

    updated to NFTUP
    Updated description, name, and meta to correct branding

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,15 +12,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Head>
-        <title>thirdweb Marketplace with Next.JS</title>
+        <title>NFT University Press Marketplace</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Learn How To Use Thirdweb's Marketplace with Next.JS To List Your NFTs For Sale, Accept Bids, and Buy NFTs"
+          content="Buy & Sell NFT University Press NFTBooks"
         />
         <meta
           name="keywords"
-          content="Thirdweb, Marketplace, NFT Marketplace Tutorial, NFT Auction Tutorial, How To Make OpenSea"
+          content="NFT University Press, NFT, University, Press, PageDAO"
         />
       </Head>
       <Header />
