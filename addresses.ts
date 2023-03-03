02b8d0c581dd00a214b20b3ab6510264f06f1859
rbpollock <43445728+rbpollock@users.noreply.github.com>
2023-03-03 00:27:14 -0500

    updated to nftup marketplace contract address

--- addresses.ts
+++ addresses.ts
@@ -1,2 +1,2 @@
 // Use your marketplace contract address here
-export const marketplaceContractAddress = '0xCFA7cA9fD5d32A6E9104aC849b1094Ea8c71e294';
+export const marketplaceContractAddress = '0xa9121720327dC1f78BF5fC3731a363864BD8A72b';
